00dde15b494c4ba2d517ed75ab92233c2576910b
MentaByte <nabilft@gmail.com>
2026-03-03 19:52:06 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Estrategia: Cache First con fallback a red
    =============================================== */
 
-const CACHE_NAME = 'calculadora-v1.1';
+const CACHE_NAME = 'calculadora-v1.2';
 
 const BASE = self.location.pathname.replace(/sw\.js$/, '');
 
