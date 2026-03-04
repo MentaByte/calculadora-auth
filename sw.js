c6cdc6b45a8e218c1a6b325bfd35b7e9377bb29b
MentaByte <nabilft@gmail.com>
2026-03-04 09:32:19 -0500

    Update sw.js
    optimizacion para safari

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Estrategia: Cache First con fallback a red
    =============================================== */
 
-const CACHE_NAME = 'calculadora-v1.3';
+const CACHE_NAME = 'calculadora-v1.4'; //optimizacion para safari
 
 const BASE = self.location.pathname.replace(/sw\.js$/, '');
 
